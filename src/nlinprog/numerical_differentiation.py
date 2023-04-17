e3a97b23613f3888fcf9b6c2cb4129f42f0b3e38
Andrew Muth <agmuth@gmail.com>
2023-04-17 17:10:18 -0600

    added jacobian

--- src/nlinprog/numerical_differentiation.py
+++ src/nlinprog/numerical_differentiation.py
@@ -20,3 +20,11 @@ def backward_difference(f: callable, h: Optional[float]=1e-4) -> callable:
     def f_prime(x: np.ndarray, p: np.ndarray) -> np.ndarray:
         return h_inv * (f(x) - f(x - h*p))
     return f_prime
+
+def jacobian(f: callable, method: Optional[str]="c", h: Optional[float]=1e-4) -> callable:
+    #TODO: support other derivatve schemes
+    f_prime = central_difference(f, h)
+    def jacobian(x: np.ndarray) -> np.ndarray:
+        identity_matrix = np.eye(x.shape[0])
+        return np.array([[f_prime(x, identity_matrix[:, [i]]) for i in range(x.shape[0])]]).T  #TODO: vectorize better
+    return jacobian
--- tests/test_numerical_differentiation.py
+++ tests/test_numerical_differentiation.py
@@ -5,6 +5,7 @@ from nlinprog.numerical_differentiation import (
     forward_difference,
     central_difference,
     backward_difference,
+    jacobian
 )
 
 
@@ -12,29 +13,37 @@ RTOL = 1e-3
 
 
 class NumericalDerivativeTestFunction():
-    def __init__(self, f:callable, x: np.ndarray, p: np.ndarray, f_prime_res: float) -> None:
-        self.f = f,
-        self.x = x,
-        self.p = p,
+    def __init__(self, f:callable, x: np.ndarray, p: np.ndarray, f_prime_res: float, f_jacobian: np.ndarray) -> None:
+        self.f = f
+        self.x = x
+        self.p = p
         self.f_prime_res = f_prime_res
+        self.f_jacobian = f_jacobian
         
 
 sphere_test_func = NumericalDerivativeTestFunction(
-    f=lambda x: np.dot(x, x),
-    x=np.array([1, -1]),
-    p=np.array([-1, 1]),
-    f_prime_res=-4.
+    f=lambda x: np.sum(x*x),
+    x=np.array([[1, -1]]).T,
+    p=np.array([[-1, 1]]).T,
+    f_prime_res=-4.,
+    f_jacobian=np.array([[2, -2]]).T,
 )
 
 
 @pytest.mark.parametrize("calc_derivative", [forward_difference, central_difference, backward_difference])
-@pytest.mark.parametrize("test_func", [sphere_test_func])
-def test_numerical_first_derivative(calc_derivative: callable, test_func: NumericalDerivativeTestFunction):
-    f_prime = calc_derivative(test_func.f[0])
+@pytest.mark.parametrize("func", [sphere_test_func])
+def test_numerical_first_derivative(calc_derivative: callable, func: NumericalDerivativeTestFunction):
     assert np.allclose(
-        f_prime(test_func.x[0], test_func.p[0]), # not sure why being passed in as tuples
-        test_func.f_prime_res, 
+        calc_derivative(func.f)(func.x, func.p), 
+        func.f_prime_res, 
         rtol=RTOL
     )
 
 
+@pytest.mark.parametrize("func", [sphere_test_func])
+def test_jacobian(func: NumericalDerivativeTestFunction):
+    assert np.allclose(
+        jacobian(func.f)(func.x), 
+        func.f_jacobian,
+        rtol=RTOL
+    )
