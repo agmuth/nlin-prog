9d5cdb2344dee89ac1e8db1d4c2771b3363fd21e
Andrew Muth <agmuth@gmail.com>
2023-04-20 13:02:06 -0600

    added test funcs

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -14,21 +14,61 @@ class OptimizationTestFunction():
 
 
 sphere_func_2d = OptimizationTestFunction(
-    f = lambda x: np.sum(x**2),
+    f=lambda x: np.sum(x**2),
     x_min=np.zeros(2),
     x_start=1*np.ones(2)
 )
 
 bazaraa_ex = OptimizationTestFunction(
-    f = lambda x: (x[0] - 2)**4 + (x[0] - 2*x[1])**2,
+    f=lambda x: (x[0] - 2)**4 + (x[0] - 2*x[1])**2,
     x_min=np.array([2., 1.]),
     x_start=np.array([0.0, 3.0])
 )
 
+booth_func = OptimizationTestFunction(
+    f=lambda x: (x[0] + 2*x[1] - 7)**2 + (2*x[0] + x[1] - 5)**2,
+    x_min=np.array([1., 3.]),
+    x_start=np.array([-8., -8.])
+)
+
+matyas_func = OptimizationTestFunction(
+    f=lambda x: 0.26*np.sum(x*x) - 0.48*np.prod(x),
+    x_min=np.array([0., 0.]),
+    x_start=np.array([2., -8.])
+)
+
+bulkin_no_6_func = OptimizationTestFunction(
+    f=lambda x: 100*np.sqrt(np.abs(x[1] * 0.01*x[0]**2) + 0.01*np.abs(x[0]+10)),
+    x_min=np.array([-10., 1.]),
+    x_start=np.array([-12., 3.])
+)
+
+rosebrock_10d_func = OptimizationTestFunction(
+    f=lambda x: 100*np.sum((x[1:] - x[:-1]**2)**2 + (1 - x[:-1]**2)),
+    x_min=np.ones(10),
+    x_start=-5*np.ones(1)
+)
+
+goldstien_price_func = OptimizationTestFunction(
+    f=lambda x: (
+        (
+            1 
+            + (x.sum() + 1)**2 
+            * (19 - 14*x[0] + 3*x[0]**2 - 14*x[1] + 6*x.prod() + 3*x[1]**2) 
+        )
+        * (
+            30
+            + (2*x[0] - 3*x[1])**2
+            * (18 - 32*x[0] + 12*x[0]**2 + 48*x[1] - 36*x.prod() + 27*x[1]**2) 
+        ) 
+    ),
+    x_min=np.array([0., -1.]),
+    x_start=np.array([-2., 2.])
+)
 
 
 @pytest.mark.parametrize("solver", [steepest_descent])
-@pytest.mark.parametrize("func", [sphere_func_2d, bazaraa_ex])
+@pytest.mark.parametrize("func", [sphere_func_2d, bazaraa_ex, booth_func, matyas_func, rosebrock_10d_func, goldstien_price_func])
 @pytest.mark.parametrize("line_search", [armijo_backtracking_line_search, wolfe_zoom_line_search])
 @pytest.mark.parametrize("atol", [1e-4])
 def test_sovlers_atol(solver: callable, func: OptimizationTestFunction, line_search, atol):
