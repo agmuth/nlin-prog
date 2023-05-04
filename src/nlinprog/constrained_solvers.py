a01278194b77ea404e2fc0c7b0077f9c84d84122
andrew muth <agmuth@gmail.com>
2023-05-04 11:56:12 -0600

    added penalized cg method

--- src/nlinprog/constrained_solvers.py
+++ src/nlinprog/constrained_solvers.py
@@ -7,7 +7,7 @@ from nlinprog.line_search import LINE_SEARCH_MAPPING, LineSearch
 from nlinprog.inverse_hessian import APPROX_INVERSE_HESSIAN_MAPPING, ApproxInverseHessian
 from nlinprog.conjugate_gradient_direction import CALC_CONJUGATE_GRADIENT_DIRECTION_MAPPING, CalcConjugateGradientDirection
 from nlinprog.utils import SimpleConvergenceTest, build_result_object
-from nlinprog.unconstrained_solvers import QuasiNewtonMethod
+from nlinprog.unconstrained_solvers import QuasiNewtonMethod, ConjugateGradientMethod
 from nlinprog.penalty_functions import INEQUALITY_PENALTY_FUNCTIONS_MAPPING, EQUALITY_PENALTY_FUNCTIONS_MAPPING, InequalityPenaltyFunction, EqualityPenaltyFunction, zero_func
 
 
@@ -18,7 +18,7 @@ class ConstrainedSolver(ABC):
         pass
 
 
-class PenalizedQuasiNewtonMethod(ConstrainedSolver, QuasiNewtonMethod):
+class PenalizedQuasiNewtonMethod(ConstrainedSolver):
     def __init__(self, f: callable, g: Optional[callable]=None, h: Optional[callable]=None, g_penalty: Optional[Union[InequalityPenaltyFunction, str]]="relu-squared", h_penalty: Optional[Union[EqualityPenaltyFunction, str]]="squared", line_search_method: Union[LineSearch, str]="wolfe", inverse_hessian_method: Union[ApproxInverseHessian, str]="exact"):
         self.f = f
         self.g = g
@@ -28,7 +28,6 @@ class PenalizedQuasiNewtonMethod(ConstrainedSolver, QuasiNewtonMethod):
         self.solver = QuasiNewtonMethod(f=f, line_search_method=line_search_method, inverse_hessian_method=inverse_hessian_method)
 
     def solve(self, x_0: np.ndarray, mu: float=2.0, beta: float=2.0, penalty_atol: Optional[float]=1e-4, maxiters1: int=200, maxiters2: int=200, grad_atol: Optional[float]=1e-4):
-        # move to ABC?
         # init params for algorithm
         x_k = np.array(x_0).astype(np.float64)
         penalty_func = lambda x: self.g_penalized(x) + self.h_penalized(x)
@@ -53,6 +52,16 @@ class PenalizedQuasiNewtonMethod(ConstrainedSolver, QuasiNewtonMethod):
         return build_result_object(self.f, x_k, k, converged)
     
 
+class PenalizedConjugateGradientMethod(PenalizedQuasiNewtonMethod):
+    def __init__(self, f: callable, g: Optional[callable]=None, h: Optional[callable]=None, g_penalty: Optional[Union[InequalityPenaltyFunction, str]]="relu-squared", h_penalty: Optional[Union[EqualityPenaltyFunction, str]]="squared", line_search_method: Union[LineSearch, str]="wolfe", conjugate_gradient_direction_method: Union[CalcConjugateGradientDirection, str]="fletcher-reeves"):
+        self.f = f
+        self.g = g
+        self.h = h
+        self.g_penalized = zero_func if not g else INEQUALITY_PENALTY_FUNCTIONS_MAPPING[g_penalty]()(g) if isinstance(g_penalty, str) else g_penalty(g) 
+        self.h_penalized = zero_func if not h else INEQUALITY_PENALTY_FUNCTIONS_MAPPING[h_penalty]()(h) if isinstance(h_penalty, str) else h_penalty(h) 
+        self.solver = ConjugateGradientMethod(f=f, line_search_method=line_search_method, conjugate_gradient_direction_method=conjugate_gradient_direction_method)
+
+
 if __name__ == "__main__":
     # rosenbrock constrained with cubic and a line
     f=lambda x: (1 - x[0])**2 + 100*(x[1] - x[0]**2)**2
@@ -62,7 +71,7 @@ if __name__ == "__main__":
     ])
     x_start=np.array([1.0, -0.5])
 
-    solver = PenalizedQuasiNewtonMethod(f=f, g=g)
+    solver = PenalizedConjugateGradientMethod(f=f, g=g)
     res = solver.solve(x_0=x_start)
 
     print(res)
